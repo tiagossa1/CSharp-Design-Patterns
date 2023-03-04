c136d0904beda4028d93240e2ddefb6a5c40aec8
tiagossa1 <tiagossa.2000@gmail.com>
2023-03-04 20:11:29 +0000

    + Added small comment on LSP.

--- SOLID/LSP/Program.cs
+++ SOLID/LSP/Program.cs
@@ -2,6 +2,8 @@
 
 using LSP.Payments;
 
+// By using Liskov substitution principle, we are able to remove or re-use CreditCard, DebitCard or BarclaysRewards based on business logic without breaking the program since all of the classes inherit from IPaymentInstrument interface.
+// You can test it by uncomment the following lines - both have Validate and Collect methods implemented.
 // var card = new CreditCard();
 // var card = new DebitCard();
 var card = new BarclaysRewards();
